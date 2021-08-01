459a5c49fc0fe9db4a60bc07aff342226a09009a
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2021-08-01 16:55:56 +0400

    App complite

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
-<template>
-  <div id="app">
-
+<template xmlns="">
+  <div class="container">
+    <app-header :quote-count="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import NewQuote from "./components/NewQuote.vue";
+import QuoteGrid from "./components/QuoteGrid.vue"
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
   components: {
-
-  }
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  data: function () {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote)
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1)
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
